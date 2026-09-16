904d8d1d3a453123a5ab0758748af899f7180b37
UMESH KUMAR GOYAL <90444981+goyalper@users.noreply.github.com>
2026-09-17 00:22:54 +0530

    Data types V1.1

---
+++ DataTypesInJava/datatypeInJava.java
@@ -0,0 +1,41 @@
+package DataTypesInJava;
+
+public class datatypeInJava {
+
+    public static void main(String[] args) {
+        /*
+         ** Primitive datatypes
+         * Integers
+         * int a = 5;
+         * short b = 10;
+         * long c = 15L;
+         * byte d = 20;
+         * 
+         * Floating point numbers
+         * float e = 5.5f;
+         * double f = 10.5;
+         * 
+         * Character
+         * char g = 'A';
+         * 
+         * boolean
+         * boolean h = true;
+         * 
+         * 
+         * * non-primitive datatypes
+         * String
+         * arrays
+         * list
+         * set
+         * stack
+         * vector
+         * dictionary
+         * all user defined classes
+         * etc
+         */
+
+        System.out.println("Hello World");
+
+    }
+
+}
