d2136f0d50d1a25be1c75d0297e565ac72bd37a9
UMESH KUMAR GOYAL <90444981+goyalper@users.noreply.github.com>
2026-09-18 23:39:01 +0530

    Star Pattern

---
+++ Printing/star.java
@@ -0,0 +1,12 @@
+package Printing;
+
+public class star{
+    public static void main(String a[]){
+        for(int i = 0; i<30; i++)
+        {for(int j = 0; j<8; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
