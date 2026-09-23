7d1ce7d8c2a4e70696466f771c4d914a81ba3ee0
UMESH KUMAR GOYAL <90444981+goyalper@users.noreply.github.com>
2026-09-23 23:35:43 +0530

    foreach Loop

---
+++ Loops/foreachLoop.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class foreachLoop{
+    
+    public static void main(String[] args) {
+        int arr[][]=new int [5][3];
+        java.util.Random r = new java.util.Random();
+        
+        for(int[] row:arr){
+            for(int j=0;j<row.length;j++){
+                row[j]=r.nextInt(100);
+            }
+        }
+
+        for(int[] row:arr){
+            for(int value:row){
+                System.out.print(value+" ");
+            }
+            System.out.println();
+        }
+    }
+}
